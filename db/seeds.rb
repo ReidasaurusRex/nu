227555fa19e985e8c6ea277cabbd1ee344ef5579
ReidasaurusRex <reidhlewis91@gmail.com>
2016-02-11 11:25:23 -0700

    Added team to users and profiles in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+jarrell = User.create(email: "jarrell@nu.world", password: "1234");
+kendra = User.create(email: "kendra@nu.world", password: "1234");
+steven = User.create(email: "steven@nu.world", password: "1234");
+reid = User.create(email: "reid@nu.world", password: "1234");
+
+jprof = Profile.create(user_id: jarrell.id, first_name: "Jarrell", last_name: "Whipple", state: "ut", interests: "Saving the planet");
+kprof = Profile.create(user_id: kendra.id, first_name: "Kendra", last_name: "Black", state: "ut", interests: "Saving the planet");
+sprof = Profile.create(user_id: steven.id, first_name: "Steven", last_name: "Nagie", state: "ut", interests: "Saving the planet");
+rprof = Profile.create(user_id: reid.id, first_name: "Reid", last_name: "Lewis", state: "az", interests: "Saving the planet. And riding my bike");
