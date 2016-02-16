bff9c3ad7f8cb31e8fd65f732942be9f08627598
ReidasaurusRex <reidhlewis91@gmail.com>
2016-02-15 21:21:57 -0700

    Began defining follow/subscribe/unfollow/unsubscribe link in profile helper

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -2,4 +2,8 @@ module ProfilesHelper
   def state_list
       [["AK", "ak"], ["AL", "al"], ["AR", "ar"], ["AZ", "az"], ["CA", "ca"], ["CO", "co"], ["CT", "ct"], ["DC", "dc"], ["DE", "de"], ["FL", "fl"], ["GA", "ga"], ["HI", "hi"], ["IA", "ia"], ["ID", "id"], ["IL", "il"], ["IN", "in"], ["KS", "ks"], ["KY", "ky"], ["LA", "la"], ["MA", "ma"], ["MD", "md"], ["ME", "me"], ["MI", "mi"], ["MN", "mn"], ["MO", "mo"], ["MS", "ms"], ["MT", "mt"], ["NC", "nc"], ["ND", "nd"], ["NE", "ne"], ["NH", "nh"], ["NJ", "nj"], ["NM", "nm"], ["NV", "nv"], ["NY", "ny"], ["OH", "oh"], ["OK", "ok"], ["OR", "or"], ["PA", "pa"], ["RI", "ri"], ["SC", "sc"], ["SD", "sd"], ["TN", "tn"], ["TX", "tx"], ["UT", "ut"], ["VA", "va"], ["VT", "vt"], ["WA", "wa"], ["WI", "wi"], ["WV", "wv"], ["WY", "wy"]]
   end
+
+  def follow_subscribe_link(user_profile, target_profile)
+    
+  end
 end
