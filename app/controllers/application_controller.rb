cb6d32f28982fcc3fd14c246fe7cbdc31c249291
ReidasaurusRex <reidhlewis91@gmail.com>
2016-02-18 10:07:23 -0700

    Removed superfluous functions from application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,25 +12,4 @@ class ApplicationController < ActionController::Base
   def check_for_log_in
     @logged_in = true if session[:user_id]
   end
-
-  def get_profile_from_profile_id
-    @profile = Profile.find(params[:profile_id])
-  end
-
-  def get_emissions_profile_from_emission_profile_id
-    @emissions_profile = EmissionsProfile.find(params[:emissions_profile_id])
-  end
-
-  def ensure_profile_is_users
-    user = User.find(session[:user_id])
-    if user.profile
-      unless @profile == user.profile
-        flash[:error] = "You can't access that"
-        redirect_to user_profile_path(user_id: session[:user_id], id: user.profile.id)
-      end
-    else
-      flash[:error] = "Please create a profile"
-      redirect_to new_user_profile_path(user_id: user.id)
-    end
-  end
 end
