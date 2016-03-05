ba5a0e6c03c4c56201dd9d33849941eb171d09b6
ReidasaurusRex <reidhlewis91@gmail.com>
2016-03-05 08:32:21 -0700

    Changed diets controller for rename

--- app/controllers/profile_matches_user/calculator_component/diets_controller.rb
+++ app/controllers/profile_matches_user/calculator_component/diets_controller.rb
@@ -1,14 +1,14 @@
-class ProfileMatchesUser::CalculatorComponent::DietProfilesController < ProfileMatchesUser::CalculatorComponentsController
+class ProfileMatchesUser::CalculatorComponent::DietsController < ProfileMatchesUser::CalculatorComponentsController
   before_action :get_footprint
-  before_action :get_diet_profile, except: [:new, :create]
+  before_action :get_diet, except: [:new, :create]
   before_action :ensure_footprint_belongs_to_user
 
   def new
-    @diet_profile = DietProfile.new
+    @diet = Diet.new
   end
 
   def create
-    create_diet_profile(diet_params)
+    create_diet(diet_params)
   end
 
   def show
@@ -18,43 +18,43 @@ class ProfileMatchesUser::CalculatorComponent::DietProfilesController < ProfileM
   end
 
   def update
-    update_diet_profile(diet_params)
+    update_diet(diet_params)
   end
 
   def destroy
   end
 
   private
-  def get_diet_profile
-    @diet_profile = DietProfile.find(params[:id])
+  def get_diet
+    @diet = Diet.find(params[:id])
   end
 
   def diet_params
-    params.fetch(:diet_profile, {}).permit(:diet_type).merge(footprint_id: params[:footprint_id])
+    params.fetch(:diet, {}).permit(:diet_type).merge(footprint_id: params[:footprint_id])
   end
 
-  def create_diet_profile(params)
-    @diet_profile = DietProfile.new(params)
-    if @diet_profile.save
-      emissions = @diet_profile.calculate_emissions
-      @diet_profile.update(section_emissions: emissions)
+  def create_diet(params)
+    @diet = Diet.new(params)
+    if @diet.save
+      emissions = @diet.calculate_emissions
+      @diet.update(section_emissions: emissions)
       @footprint.update(diet_emissions: emissions)
       @footprint.check_for_completion
       flash[:calculator_message] = "Diet emissions: #{emissions}lbs CO2e"
-      redirect_to new_footprint_water_profile_path(@footprint)
+      redirect_to new_footprint_water_path(@footprint)
     else
       render :new
     end
   end
 
-  def update_diet_profile(params)
-    if @diet_profile.update(params)
-      emissions = @diet_profile.calculate_emissions
-      @diet_profile.update(section_emissions: emissions)
+  def update_diet(params)
+    if @diet.update(params)
+      emissions = @diet.calculate_emissions
+      @diet.update(section_emissions: emissions)
       @footprint.update(diet_emissions: emissions)
       @footprint.check_for_completion
       flash[:calculator_message] = "Diet emissions: #{emissions}lbs CO2e"
-      redirect_to new_footprint_water_profile_path(@footprint)
+      redirect_to new_footprint_water_path(@footprint)
     else
       render :edit
     end
