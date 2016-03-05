ab3748aedb6c391b9ddf1869a7545dbd45616abb
ReidasaurusRex <reidhlewis91@gmail.com>
2016-03-05 08:29:40 -0700

    Changed air_travels_controller to remove 'profile'

--- app/controllers/profile_matches_user/calculator_component/air_travels_controller.rb
+++ app/controllers/profile_matches_user/calculator_component/air_travels_controller.rb
@@ -1,10 +1,10 @@
-class ProfileMatchesUser::CalculatorComponent::AirTravelProfilesController < ProfileMatchesUser::CalculatorComponentsController
-  before_action :get_transportation_profile
+class ProfileMatchesUser::CalculatorComponent::AirTravelsController < ProfileMatchesUser::CalculatorComponentsController
+  before_action :get_transportation
   before_action :get_transportation_footprint
-  before_action :get_air_travel_profile, except: [:new, :create]
+  before_action :get_air_travel, except: [:new, :create]
 
   def new
-    @air_travel_profile = AirTravelProfile.new
+    @air_travel = AirTravel.new
   end
 
   def create
@@ -23,19 +23,19 @@ class ProfileMatchesUser::CalculatorComponent::AirTravelProfilesController < Pro
   end
 
   private
-  def get_transportation_profile
-    @transportation_profile = TransportationProfile.find(params[:transportation_profile_id])
+  def get_transportation
+    @transportation = Transportation.find(params[:transportation_id])
   end
 
   def get_transportation_footprint
-    @footprint = @transportation_profile.footprint
+    @footprint = @transportation.footprint
   end
 
-  def get_air_travel_profile
-    @air_travel_profile = AirTravelProfile.find(params[:id])
+  def get_air_travel
+    @air_travel = AirTravel.find(params[:id])
   end
 
-  def air_travel_profile_params
-    params.fetch(:air_travel_profile, {}).permit(:d_economy, :d_business, :i_economy, :i_premium_economy, :i_business, :i_first).merge(transportation_profile_id: @transportation_profile.id)
+  def air_travel_params
+    params.fetch(:air_travel, {}).permit(:d_economy, :d_business, :i_economy, :i_premium_economy, :i_business, :i_first).merge(transportation_id: @transportation.id)
   end 
 end
