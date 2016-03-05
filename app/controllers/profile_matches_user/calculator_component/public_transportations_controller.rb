5450c79d06130570e1d98d04807c7803e18f4453
ReidasaurusRex <reidhlewis91@gmail.com>
2016-03-05 08:34:46 -0700

    Changed public_transportations controller for rename

--- app/controllers/profile_matches_user/calculator_component/public_transportations_controller.rb
+++ app/controllers/profile_matches_user/calculator_component/public_transportations_controller.rb
@@ -1,14 +1,14 @@
 class ProfileMatchesUser::CalculatorComponent::PublicTransportationProfilesController < ProfileMatchesUser::CalculatorComponentsController
-  before_action :get_transportation_profile
+  before_action :get_transportation
   before_action :get_transportation_footprint
-  before_action :get_public_transportation_profile, except: [:new, :create]
+  before_action :get_public_transportation, except: [:new, :create]
 
   def new
-    @public_transportation_profile = PublicTransportationProfile.new
+    @public_transportation = PublicTransportation.new
   end
 
   def create
-    create_public_transportation_profile(public_transportation_profile_params)
+    create_public_transportation(public_transportation_params)
   end
 
   def show
@@ -24,32 +24,32 @@ class ProfileMatchesUser::CalculatorComponent::PublicTransportationProfilesContr
   end
 
   private
-  def get_transportation_profile
-    @transportation_profile = TransportationProfile.find(params[:transportation_profile_id])
+  def get_transportation
+    @transportation = Transportation.find(params[:transportation_id])
   end
 
   def get_transportation_footprint
-    @footprint = @transportation_profile.footprint
+    @footprint = @transportation.footprint
   end
 
-  def get_public_transportation_profile
-    @public_transportation_profile = PublicTransportationProfile.find(params[:id])
+  def get_public_transportation
+    @public_transportation = PublicTransportation.find(params[:id])
   end
 
-  def public_transportation_profile_params
-    params.fetch(:public_transportation_profile, {}).permit(:use, :metro, :commuter, :inter_city, :bus, :taxi).merge(transportation_profile_id: @transportation_profile.id)
+  def public_transportation_params
+    params.fetch(:public_transportation, {}).permit(:use, :metro, :commuter, :inter_city, :bus, :taxi).merge(transportation_id: @transportation.id)
   end
 
-  def create_public_transportation_profile(params)
-    @public_transportation_profile = PublicTransportationProfile.new(params)
-    if @public_transportation_profile.save
-      emissions = @public_transportation_profile.calculate_emissions
-      @public_transportation_profile.update(sub_section_emissions: emissions)
-      @transportation_profile.update(public_emissions: emissions)
-      @transportation_profile.check_for_completion
-      @transportation_profile.calculate_emissions
+  def create_public_transportation(params)
+    @public_transportation = PublicTransportation.new(params)
+    if @public_transportation.save
+      emissions = @public_transportation.calculate_emissions
+      @public_transportation.update(sub_section_emissions: emissions)
+      @transportation.update(public_emissions: emissions)
+      @transportation.check_for_completion
+      @transportation.calculate_emissions
       flash[:calculator_message] = "Public transportation emissions: #{emissions}lbs CO2e"
-      redirect_to new_transportation_profile_air_travel_profile_path(transportation_profile_id: @transportation_profile.id)
+      redirect_to new_transportation_air_travel_path(transportation_id: @transportation.id)
     else
       render :new
     end
