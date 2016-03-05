9740b73ef8835fa2f92159f995b86aa69d50f8ae
ReidasaurusRex <reidhlewis91@gmail.com>
2016-03-05 08:47:24 -0700

    Changed footprint model to reflect rename

--- app/models/footprint.rb
+++ app/models/footprint.rb
@@ -1,10 +1,10 @@
 class Footprint < ActiveRecord::Base
   belongs_to :profile 
-  has_one :water_profile, dependent: :destroy
-  has_one :diet_profile, dependent: :destroy
-  has_one :waste_profile, dependent: :destroy
-  has_one :transportation_profile, dependent: :destroy
-  has_one :home_energy_profile, dependent: :destroy
+  has_one :water, dependent: :destroy
+  has_one :diet, dependent: :destroy
+  has_one :waste, dependent: :destroy
+  has_one :transportation, dependent: :destroy
+  has_one :home_energy, dependent: :destroy
 
   def check_for_completion
     if self.total_emissions &&  self.water_emissions && self.diet_emissions && self.transportation_emissions && self.waste_emissions && self.home_energy_emissions
