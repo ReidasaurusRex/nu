fc408ac844740412c0bb3550943970f20b26d4b4
ReidasaurusRex <reidhlewis91@gmail.com>
2016-03-06 21:57:19 -0700

    Removed unneccessary files for habit/challenge restructure. Renamed files and folders. Need to change classes and routes

--- app/models/started_challenge.rb
+++
@@ -1,5 +0,0 @@
-class StartedChallenge < ActiveRecord::Base
-  belongs_to :profile
-  belongs_to :challenge 
-  belongs_to :progress_category
-end
--- app/models/started_habit.rb
+++
@@ -1,5 +0,0 @@
-class StartedHabit < ActiveRecord::Base
-  belongs_to :profile
-  belongs_to :habit 
-  belongs_to :progress_category
-end
