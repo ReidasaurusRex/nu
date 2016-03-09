c506d1692d0ce80e2c91017d7702f1f2ece3cf6e
ReidasaurusRex <reidhlewis91@gmail.com>
2016-03-09 08:44:01 -0700

    Removed beta_user files. Extracted to separate app

--- app/models/beta_user.rb
+++
@@ -1,9 +0,0 @@
-class BetaUser < ActiveRecord::Base
-  validates :email, presence: {message: " is required"}
-  validates :first_name, presence: {message: " is required"}
-  validates :last_name, presence: {message: " is required"}
-  validates :email, uniqueness: {message: " has already been used"}
-  validates :communicate, inclusion: {in: [*1..10], 
-    message: "Please select one through ten"}
-  validates :explanation, presence: {message: "Please enter an explanation"}
-end
