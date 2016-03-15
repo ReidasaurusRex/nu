2e41f3df4bbc9d83aae1b9c56e041dc24292332e
ReidasaurusRex <reidhlewis91@gmail.com>
2016-03-14 17:01:39 -0700

    Added logo colors to mixins.scss

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,9 +1,10 @@
 var ready = function() {
   var counter = 0;
   $(".dropdown-menu-button").click(function(event) {
-    counter ++;
-    console.log("Counter at: " + counter);
     $(".dropdown-menu").toggleClass("expanded-menu");
+  });
+  $(".main-body").click(function(event) {
+    $(".dropdown-menu").removeClass("expanded-menu");
   });
 };
 
