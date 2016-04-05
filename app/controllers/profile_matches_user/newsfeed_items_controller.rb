1c81ea5b841be4acc1ed751e6f05674e356fe6f9
ReidasaurusRex <reidhlewis91@gmail.com>
2016-04-05 11:30:06 -0700

    Created a newsfeed overview. Still need create and push functionality for newsfeed items.

--- app/controllers/profile_matches_user/newsfeed_items_controller.rb
+++ app/controllers/profile_matches_user/newsfeed_items_controller.rb
@@ -1,6 +1,7 @@
 class ProfileMatchesUser::NewsfeedItemsController < Inheritance::ProfileMatchesUserController
   before_action :get_profile_from_profile_id
   def index
+    @newsfeed_items = @profile.newsfeed_items
   end
 
   def show
--- app/models/newsfeed_item.rb
+++ app/models/newsfeed_item.rb
@@ -1,3 +1,19 @@
 class NewsfeedItem < ActiveRecord::Base
   belongs_to :profile
+  belongs_to :source, class_name: "Profile"
+
+  def time_since_creation
+    time_array = [[3153600 ,"year"], [2592000, "month"], [86400, "day"], [3600, "hour"], [60, "min"], [1, "sec"]]
+    time_since = Time.now - self.created_at
+    time_array.each do |time| 
+      # unless time[0] == 1
+        if (time_since / time[0]) >= 1
+          x_since = (time_since / time[0]).to_i
+          response = "#{x_since} #{time[1]}"
+          response << "s" if x_since > 1
+          return response
+        end
+      # end
+    end
+  end
 end
