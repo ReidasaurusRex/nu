16fe8e12f5162ef528c55bc6a0831dc7e2dec0e0
ReidasaurusRex <reidhlewis91@gmail.com>
2016-04-16 11:19:38 -0700

    Set up basic html and rough stylings for site views

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,6 @@
 class AboutController < ApplicationController
+  before_action :get_profile_if_logged_in
+
   def general
   end
 
@@ -12,5 +14,13 @@ class AboutController < ApplicationController
   end
 
   def future
+  end
+
+  private
+
+  def get_profile_if_logged_in
+    if session[:user_id]
+      @profile = User.find(session[:user_id]).profile
+    end
   end
 end
