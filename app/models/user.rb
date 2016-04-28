c6e754dd6fd015a94ee23e7d7d394dd3e4115432
ReidasaurusRex <reidhlewis91@gmail.com>
2016-04-28 09:29:54 -0700

    Changed logo to id instead of class. Added an after_create method in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ActiveRecord::Base
   validates :email, presence: {message: " is required"}
   validates :password, presence: {message: " is required"}
   validates :password, confirmation: {message: " must match"}
+
+  after_create :send_welcome_email_to_user
+
+  def send_welcome_email_to_user
+    UserMailer.welcome_email(self).deliver_later
+  end
 end
