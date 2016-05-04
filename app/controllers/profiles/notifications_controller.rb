f62a4a365b6756519b6a846a690c45b545f0e8a0
ReidasaurusRex <reidhlewis91@gmail.com>
2016-05-04 09:53:02 -0700

    Added gathering functions to notifications controller

--- app/controllers/profiles/notifications_controller.rb
+++ app/controllers/profiles/notifications_controller.rb
@@ -1,8 +1,10 @@
 class Profiles::NotificationsController < Inheritance::CompletedProfileController
   before_action :get_profile_from_profile_id
   def index
+    @notifications = @profile.notifications
   end
 
   def destroy
+    @notification = Notification.find(params[:id])
   end
 end
