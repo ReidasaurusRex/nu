b54e6b56fe41e02bb4021da3057256324630408a
ReidasaurusRex <reidhlewis91@gmail.com>
2016-05-05 10:55:35 -0700

    Added a profile pic column to profiles table. Addes uploadcare.scss to main.scss

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160501210302) do
+ActiveRecord::Schema.define(version: 20160505173625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -164,9 +164,10 @@ ActiveRecord::Schema.define(version: 20160501210302) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "interests"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "state"
+    t.string   "profile_pic"
   end
 
   create_table "public_transportations", force: :cascade do |t|
