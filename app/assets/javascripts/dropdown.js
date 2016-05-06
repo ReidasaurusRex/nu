2291f729b157da6cb408529261dd2f634ccb7a9f
ReidasaurusRex <reidhlewis91@gmail.com>
2016-05-06 09:28:14 -0700

    Extracted dropdown menu styling. Need to create separate stylesheet for profile-dropdown-menu, need to change filename on dropdown_menu and notfication_menu

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,7 +1,10 @@
 var ready = function() {
   var counter = 0;
-  $(".dropdown-menu-button").click(function(event) {
-    $(".dropdown-menu").toggleClass("expanded-menu");
+  $(".profile-dropdown-menu-button").click(function(event) {
+    $(".profile-dropdown-menu").toggleClass("expanded-menu");
+  });
+  $(".notifications-menu-button").click(function(event) {
+    $(".notifications-list").toggleClass("expanded-menu");
   });
   $(".main-body").click(function(event) {
     $(".dropdown-menu").removeClass("expanded-menu");
