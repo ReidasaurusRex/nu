4489f9710c3751a767309c5b1d2b9010376f5d3a
ReidasaurusRex <reidhlewis91@gmail.com>
2016-05-07 14:24:13 -0700

    Added javascript to manage dropdown hide on body click

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,15 +1,33 @@
 var ready = function() {
   var counter = 0;
-  $(".profile-dropdown-menu-button").click(function(event) {
-    $(".profile-dropdown-menu").toggleClass("expanded-menu");
+  $('.profile-dropdown-menu-button').click(toggleProfileMenu);
+  $('.notifications-menu-button').click(toggleNotificationsMenu);
+  $('body').click(function(event) {
+    if ($(event.target).closest('.dropdown-menu').length === 0 && $(event.target).closest('.dropdown-menu-button').length === 0) {
+        console.log('Hey!');
+        $('.dropdown-menu').removeClass('expanded-menu');
+    } else if ($(event.target).closest('.dropdown-menu').length === 1) {
+      console.log('Yup!');
+    }
   });
-  $(".notifications-menu-button").click(function(event) {
-    $(".notifications-list").toggleClass("expanded-menu");
-  });
-  $(".main-body").click(function(event) {
-    $(".dropdown-menu").removeClass("expanded-menu");
-  });
+  
 };
-
 $(document).ready(ready);
 $(document).on('page:load', ready);
+
+function toggleProfileMenu(event) {
+  $('.notifications-list').removeClass('expanded-menu');
+  $('.profile-dropdown-menu').toggleClass('expanded-menu'); 
+}
+
+function toggleNotificationsMenu(event) {
+  $('.profile-dropdown-menu').removeClass('expanded-menu');
+  $('.notifications-list').toggleClass('expanded-menu');
+}
+
+function hideDropdownMenuOnOtherClick(event) {
+  if ($(event.target).closest('.dropdown-menu').length === 0 && $(evet.target).closest('.dropdown-menu-button').length === 0) {
+    $('.dropdown-menu').removeClass('expanded-menu');
+  }
+}
+
