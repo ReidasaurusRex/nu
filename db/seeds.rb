2d6fd8be6eff53e4a6c9c8c68904d499408ab42e
ReidasaurusRex <reidhlewis91@gmail.com>
2016-05-18 08:55:54 -0700

    Added started_challenge_updates to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -65,8 +65,21 @@ challenges = Challenge.create([
 
 rprof.profile_started_challenges.create([
   {challenge_id: challenges[0].id},
-  {challenge_id: challenges[3].id}
+  {challenge_id: challenges[3].id},
+  {challenge_id: challenges[-2].id}
   ])
+
+rprof.profile_started_challenges[0].started_challenge_updates.create([
+  {challenge_id: challenges[0], difficulty: 1, update_number: 1},
+  {challenge_id: challenges[0], difficulty: 1, update_number: 2}
+  ])
+
+rprof.profile_started_challenges[1].started_challenge_updates.create([
+  {challenge_id: challenges[3].id, difficulty: 3, update_number: 1},
+  {challenge_id: challenges[3].id, difficulty: 2, update_number: 2},
+  {challenge_id: challenges[3].id, difficulty: 1, update_number: 3}
+  ])
+
 
 rprof.footprints.create([
   {total_emissions: 5000, water_emissions: 100, diet_emissions: 685, transportation_emissions: 3561, waste_emissions: 100, home_energy_emissions: 554},
