c3408f7f0321361a2a056be4760980fc4a77aebc
ReidasaurusRex <reidhlewis91@gmail.com>
2016-05-18 11:41:16 -0700

    Changed function in profile_completed_challenge to be in verb-like syntax

--- app/models/profile_completed_challenge.rb
+++ app/models/profile_completed_challenge.rb
@@ -2,9 +2,9 @@ class ProfileCompletedChallenge < ActiveRecord::Base
   belongs_to :profile
   belongs_to :challenge
 
-  after_create :add_points_from_completed_challenge
+  after_create :add_points_for_completing_challenge
 
-  def add_points_from_completed_challenge
+  def add_points_for_completing_challenge
     self.profile.point_overview.add_points(self.challenge.points, self.challenge.progress_category)
   end
 end
