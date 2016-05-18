47cd5a4f7628ea76fb55f831fb307fa790ea64c0
ReidasaurusRex <reidhlewis91@gmail.com>
2016-05-18 08:18:51 -0700

    Cleaned up old challenge migrations

--- db/migrate/20160307044853_restructure_habits_and_challenges.rb
+++ db/migrate/20160307044853_restructure_habits_and_challenges.rb
@@ -1,12 +1,7 @@
-class RestructureHabitsAndChallenges < ActiveRecord::Migration
+class RestructureHabits < ActiveRecord::Migration
   def change
-    drop_table :started_challenges
     drop_table :started_habits
-
-    rename_table :completed_challenges, :profile_challenges
     rename_table :completed_habits, :profile_habits
-
-    add_column :profile_challenges, :completed, :boolean
     add_column :profile_habits, :completed, :boolean
   end
 end
