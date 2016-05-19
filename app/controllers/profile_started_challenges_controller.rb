1608eb11332841fe85d41459cb8904e817ef32f5
ReidasaurusRex <reidhlewis91@gmail.com>
2016-05-19 12:24:44 -0700

    Added controllers and deleted unncessary files for profile_started_challenges, started_challenge_updates, and profile_completed_challenges

---
+++ app/controllers/profile_started_challenges_controller.rb
@@ -0,0 +1,10 @@
+class ProfileStartedChallengesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
---
+++ app/controllers/started_challenge_updates_controller.rb
@@ -0,0 +1,7 @@
+class StartedChallengeUpdatesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
