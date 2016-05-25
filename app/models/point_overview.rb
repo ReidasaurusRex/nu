79730a753549c05bdd8c856dea84e42f854d8a11
ReidasaurusRex <reidhlewis91@gmail.com>
2016-05-25 11:10:13 -0700

    Added section percentages for profile_score_overview

--- app/models/point_overview.rb
+++ app/models/point_overview.rb
@@ -10,4 +10,8 @@ class PointOverview < ActiveRecord::Base
   def add_score_from_footprint
     self.update(total: self.total + 10, diet: self.diet + 2, waste: self.waste + 2, water: self.water + 2, transportation: self.transportation + 2, home_energy: self.home_energy + 2)  
   end
+
+  def section_percent(section)
+    return ((self[section].to_f / self.total) * 100).round
+  end
 end
