c44363a0c6dc92aa41b95f9e785866b426e11080
ReidasaurusRex <reidhlewis91@gmail.com>
2016-05-26 21:13:01 -0700

    Restructured javascript inclusions so that point_graph only gets included after stats page

--- app/assets/javascripts/point_graph.js
+++ app/assets/javascripts/point_graph.js
@@ -1,30 +1,32 @@
 function ready() {
-  var data = $('#point-overview-graph').data('pointOverview');
-  console.log(data);
-  drawPointGraph(data);
+  var pointData = $('#point-overview-graph').data('pointOverview');
+  drawPointGraph(pointData);
 }
 
 $(document).ready(ready); 
 $(document).on('page:load', ready); // turbolinks friendly
 
 function drawPointGraph(data) {
-  new Highcharts.Chart({
-    chart: {
-      renderTo: 'point-overview-graph',
-      plotBackgroundColor: null,
-      plotBorderWidth: null,
-      plotShadow: false, 
-      type: 'pie'}, 
-    title: {
-      text: 'Point Overview'
-    }, 
-    colors: ['#B7DC78', '#65CBC9', '#FF8D6A', '#F2D383'],
-    series: [{
-      name: 'Points', 
-      data:
-        extractDataPoints(data)
-    }]
-  });
+  if ($('#point-overview-graph').length > 0) {
+    console.log(data);
+    new Highcharts.Chart({
+      chart: {
+        renderTo: 'point-overview-graph',
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false, 
+        type: 'pie'}, 
+      title: {
+        text: 'Point Overview'
+      }, 
+      colors: ['#B7DC78', '#65CBC9', '#FF8D6A', '#F2D383'],
+      series: [{
+        name: 'Points', 
+        data:
+          extractDataPoints(data)
+      }]
+    });
+  }
 }
 
 function extractDataPoints(data) {
