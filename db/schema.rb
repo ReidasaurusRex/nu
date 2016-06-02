8f5b5bf7351c0d52aaffcbae69b58bc65bcbde56
ReidasaurusRex <reidhlewis91@gmail.com>
2016-06-02 12:23:01 -0700

    Created feed model and migrated. Update gemfile.lock to have current rake version to resolve rake error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160520161203) do
+ActiveRecord::Schema.define(version: 20160602191946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,14 @@ ActiveRecord::Schema.define(version: 20160520161203) do
     t.integer  "section_emissions"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+  end
+
+  create_table "feeds", force: :cascade do |t|
+    t.string   "name"
+    t.string   "url"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "follows", force: :cascade do |t|
