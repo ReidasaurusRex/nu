15a87ff826f47f2455d4ceb7f0ce611e70e78ff4
ReidasaurusRex <reidhlewis91@gmail.com>
2016-06-05 19:19:24 -0700

    Added data-crop and field value for uploadcare

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   has_one :profile, dependent: :destroy
-  has_one :privacy_setting, dependent: :destroy
-  has_one :notification_setting, dependent: :destroy
-  has_one :email_setting, dependent: :destroy
+  # has_one :notification_setting, dependent: :destroy
+  # has_one :email_setting, dependent: :destroy
   has_secure_password(validations: false)
 
   validates :email, presence: {message: " is required"}
