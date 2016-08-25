8f8b980b1d91c3c0551a62266f3d9be133cac09b
ReidasaurusRex <reidhlewis91@gmail.com>
2016-08-25 14:56:59 -0700

    Added icons to challenges

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,4 +1,8 @@
 class Challenge < ActiveRecord::Base
   has_many :started_challenges
   has_many :completed_challenges
+
+  def image_path
+    "progress_icons/color/#{self.progress_category}"
+  end
 end
