dd41673b265fa68e2126702864d3d3f2911f0fbb
ReidasaurusRex <reidhlewis91@gmail.com>
2016-08-29 09:07:34 -0700

    Added an inclusion validation in tag.rb for water diet travel waste energy social

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,5 @@
 class Tag < ActiveRecord::Base
   belongs_to :taggable, polymorphic: true
+  validates :name, inlcusion: {in: %w(water diet travel waste energy social),
+    message: "Invalid tag"}
 end
