9c60c8f3e19829040803b3f3fa6762c711655c60
ReidasaurusRex <reidhlewis91@gmail.com>
2016-08-31 14:30:10 -0700

    Added a method to get profiles of leaderboards around current profile. Added images to leaderboard overview

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -1,2 +1,20 @@
 module StatsHelper
+  def leaderboards_around_profile(profile_id, range, category=:total)
+    leader_ids = PointOverview.order(category => :desc).pluck(:profile_id)
+    index = leader_ids.index(profile_id)
+    target_length = range * 2 + 1
+    if index < range
+      return leader_ids.slice(0, target_length).map{|id| Profile.find(id)}
+    elsif index >= (leader_ids.length - range)
+      return leader_ids.slice(-target_length, target_length).map{|id| Profile.find(id)}
+    else
+      return leader_ids[(index - range)..(index + range)].map{|id| Profile.find(id)}
+    end
+  end
+
+  def is_current_profile_leaderboard(profile_id, leaderboard_id)
+    if profile_id == leaderboard_id
+      return "current-profile-leaderboard"
+    end
+  end
 end
