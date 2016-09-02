9504224b00910b7a6344c5e85cb9e85b0da20d6f
ReidasaurusRex <reidhlewis91@gmail.com>
2016-09-02 09:25:03 -0700

    Dropped blocked_users table. Migrated

--- app/models/blocked_user.rb
+++ app/models/blocked_user.rb
@@ -1,3 +1,4 @@
 class BlockedUser < ActiveRecord::Base
   belongs_to :profile
+  belongs_to :blocked_profile, class_name: "Profile"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160829155010) do
+ActiveRecord::Schema.define(version: 20160902162421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,13 +27,6 @@ ActiveRecord::Schema.define(version: 20160829155010) do
     t.integer  "sub_section_emissions"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
-  end
-
-  create_table "blocked_users", force: :cascade do |t|
-    t.integer  "profile_id"
-    t.integer  "blocked_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "blog_posts", force: :cascade do |t|
