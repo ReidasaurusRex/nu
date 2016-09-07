97cead81fa33e30a57672a9d88621c49eb722223
ReidasaurusRex <reidhlewis91@gmail.com>
2016-09-07 10:12:17 -0700

    Added profile_dropdown component

--- app/assets/javascripts/globals/dropdown.js
+++ app/assets/javascripts/globals/dropdown.js
@@ -1,5 +1,5 @@
 function ready() {
-  $('.profile-dropdown-menu-button').click(toggleProfileMenu);
+  $('#jsProfDropdownButton').click(toggleProfileMenu);
   $('.notifications-menu-button').click(toggleNotificationsMenu);
   $('body').click(hideDropdownMenuOnOtherClick);
 }
@@ -7,18 +7,18 @@ $(document).ready(ready);
 $(document).on('page:load', ready); // turbolinks friendly
 
 function toggleProfileMenu(event) {
-  $('.notifications-list').removeClass('expanded-menu');
-  $('.profile-dropdown-menu').toggleClass('expanded-menu'); 
+  $('.notifications-list').removeClass('o-dropdown--is-open');
+  $('.jsProfDropdownButton').toggleClass('o-dropdown--is-open'); 
 }
 
 function toggleNotificationsMenu(event) {
-  $('.profile-dropdown-menu').removeClass('expanded-menu');
-  $('.notifications-list').toggleClass('expanded-menu');
+  $('.profile-dropdown-menu').removeClass('o-dropdown--is-open');
+  $('.notifications-list').toggleClass('o-dropdown--is-open');
 }
 
 function hideDropdownMenuOnOtherClick(event) {
   if ($(event.target).closest('.dropdown-menu').length === 0 && $(event.target).closest('.dropdown-menu-button').length === 0) {
-    $('.dropdown-menu').removeClass('expanded-menu');
+    $('.dropdown-menu').removeClass('o-dropdown--is-open');
   }
 }
 
