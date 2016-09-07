b22a0b6eb8a8b9fd37df680bf25a7449f3190dd6
ReidasaurusRex <reidhlewis91@gmail.com>
2016-09-07 13:35:36 -0700

    Added styles to challenges list. Created point_circle object

--- app/helpers/profile_started_challenges_helper.rb
+++ app/helpers/profile_started_challenges_helper.rb
@@ -1,9 +1,9 @@
 module ProfileStartedChallengesHelper
   def challenge_complete_or_check_in_link(started_challenge)
     if started_challenge.started_challenge_updates.length < 4
-      return link_to("Check in", new_profile_started_challenge_started_challenge_update_path(started_challenge.id)).html_safe
+      return  button_to("Check in", new_profile_started_challenge_started_challenge_update_path(started_challenge.id)).html_safe
     else
-      return link_to("Complete", new_profile_started_challenge_profile_completed_challenge_path(profile_started_challenge_id: started_challenge.id)).html_safe
+      return  button_to("Complete", new_profile_started_challenge_profile_completed_challenge_path(profile_started_challenge_id: started_challenge.id)).html_safe
     end
   end
 end
