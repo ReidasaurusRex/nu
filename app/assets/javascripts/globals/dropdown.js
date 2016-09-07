45d338067b654235fbef75f693fb477df04990de
ReidasaurusRex <reidhlewis91@gmail.com>
2016-09-07 10:44:01 -0700

    Added js for notifications list

--- app/assets/javascripts/globals/dropdown.js
+++ app/assets/javascripts/globals/dropdown.js
@@ -1,6 +1,6 @@
 function ready() {
   $('#jsProfDropdownButton').click(toggleProfileMenu);
-  $('.notifications-menu-button').click(toggleNotificationsMenu);
+  $('#jsNotiDropdownButton').click(toggleNotificationsMenu);
   $('body').click(hideDropdownMenuOnOtherClick);
 }
 $(document).ready(ready);
@@ -8,18 +8,19 @@ $(document).on('page:load', ready); // turbolinks friendly
 
 function toggleProfileMenu(event) {
   console.log(event);
-  $('.notifications-list').removeClass('o-dropdown--is-open');
+  $('.c-noti-dropdown').removeClass('o-dropdown--is-open');
   $('.c-prof-dropdown').toggleClass('o-dropdown--is-open'); 
 }
 
 function toggleNotificationsMenu(event) {
-  $('.profile-dropdown-menu').removeClass('o-dropdown--is-open');
-  $('.notifications-list').toggleClass('o-dropdown--is-open');
+  console.log(event);
+  $('.c-prof-dropdown').removeClass('o-dropdown--is-open');
+  $('.c-noti-dropdown').toggleClass('o-dropdown--is-open');
 }
 
 function hideDropdownMenuOnOtherClick(event) {
   if ($(event.target).closest('.dropdown-menu').length === 0 && $(event.target).closest('.dropdown-menu-button').length === 0) {
-    $('.dropdown-menu').removeClass('o-dropdown--is-open');
+    $('.o-dropdown-menu').removeClass('o-dropdown--is-open');
   }
 }
 
