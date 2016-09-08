9964b6b4efe6202c00de13a188073ae7104c265e
ReidasaurusRex <reidhlewis91@gmail.com>
2016-09-07 20:18:18 -0700

    Returned integers of variable width rather than floats

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -8,7 +8,7 @@ module StatsHelper
   def place_percentage_or_20(curr, highest)
     percent = (curr.point_overview.total.to_f / highest.point_overview.total.to_f) * 100
     if percent > 40
-      return percent
+      return percent.to_i
     else
       return 40
     end
