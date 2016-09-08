da5f39472963c08565a8598ea580a1ba0324a168
ReidasaurusRex <reidhlewis91@gmail.com>
2016-09-07 19:57:41 -0700

    Correct dropdown menu hide on rest of body click function

--- app/assets/javascripts/globals/dropdown.js
+++ app/assets/javascripts/globals/dropdown.js
@@ -7,20 +7,18 @@ $(document).ready(ready);
 $(document).on('page:load', ready); // turbolinks friendly
 
 function toggleProfileMenu(event) {
-  console.log(event);
   $('.c-noti-dropdown').removeClass('o-dropdown--is-open');
   $('.c-prof-dropdown').toggleClass('o-dropdown--is-open'); 
 }
 
 function toggleNotificationsMenu(event) {
-  console.log(event);
   $('.c-prof-dropdown').removeClass('o-dropdown--is-open');
   $('.c-noti-dropdown').toggleClass('o-dropdown--is-open');
 }
 
 function hideDropdownMenuOnOtherClick(event) {
-  if ($(event.target).closest('.dropdown-menu').length === 0 && $(event.target).closest('.dropdown-menu-button').length === 0) {
-    $('.o-dropdown-menu').removeClass('o-dropdown--is-open');
+  if ($(event.target).closest('.o-dropdown').length === 0 && $(event.target).closest('.jsDropdownButton').length === 0) {
+    $('.o-dropdown').removeClass('o-dropdown--is-open');
   }
 }
 
