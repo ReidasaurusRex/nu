95446b2c8448af5c0542202d72716edb4ac0daf5
ReidasaurusRex <reidhlewis91@gmail.com>
2016-09-07 20:17:10 -0700

    Added variable width on leaders of leaderboard dependent upon points vs high leader

--- app/controllers/profiles/stats_controller.rb
+++ app/controllers/profiles/stats_controller.rb
@@ -1,4 +1,23 @@
 class Profiles::StatsController < Inheritance::ProfileMatchesUserController
   def index
+    @leaders_around_profile = leaders_around_profile(@profile.id, 3)
+    @highest_leader = @leaders_around_profile[0][:prof]
+  end
+
+  private
+  def leaders_around_profile(profile_id, range, category=:total)
+    leader_ids = PointOverview.order(category => :desc).pluck(:profile_id)
+    index = leader_ids.index(profile_id)
+    target_length = range * 2 + 1
+    profiles_with_place = leader_ids.map.with_index do |id, ind|
+      {place: ind + 1, prof: Profile.find(id)}
+    end
+    if index < range
+      return profiles_with_place.slice(0, target_length)
+    elsif index >= (leader_ids.length - range)
+      return profiles_with_place.slice(-target_length, target_length)
+    else
+      return profiles_with_place[(index - range)..(index + range)]
+    end
   end
 end
--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -1,23 +1,16 @@
 module StatsHelper
-  def leaderboards_around_profile(profile_id, range, category=:total)
-    leader_ids = PointOverview.order(category => :desc).pluck(:profile_id)
-    index = leader_ids.index(profile_id)
-    target_length = range * 2 + 1
-    profiles_with_place = leader_ids.map.with_index do |id, ind|
-      {place: ind + 1, prof: Profile.find(id)}
-    end
-    if index < range
-      return profiles_with_place.slice(0, target_length)
-    elsif index >= (leader_ids.length - range)
-      return profiles_with_place.slice(-target_length, target_length)
-    else
-      return profiles_with_place[(index - range)..(index + range)]
-    end
-  end
-
   def is_current_profile_leader(profile_id, leaderboard_id)
     if profile_id == leaderboard_id
       return "u-profile-leader"
+    end
+  end
+
+  def place_percentage_or_20(curr, highest)
+    percent = (curr.point_overview.total.to_f / highest.point_overview.total.to_f) * 100
+    if percent > 40
+      return percent
+    else
+      return 40
     end
   end
 end
