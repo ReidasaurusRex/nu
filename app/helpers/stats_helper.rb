ab52050b6e6079b550233374837295a29c1e3751
ReidasaurusRex <reidhlewis91@gmail.com>
2016-09-13 11:07:53 -0700

    Added progress bar to emissions allowance

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -1,4 +1,12 @@
 module StatsHelper
+  def recent_footprint_allotment_percentage(footprint)
+    if footprint
+      return footprint.percent_of_us_allotment
+    else
+      return 0
+    end    
+  end
+
   def is_current_profile_leader(profile_id, leaderboard_id)
     if profile_id == leaderboard_id
       return "u-profile-leader"
