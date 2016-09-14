3a901365bea1c95738847e40b9cf8cbf0413ebd1
ReidasaurusRex <reidhlewis91@gmail.com>
2016-09-14 15:02:40 -0700

    Changed text-color utility to name color-names rather than generic names

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -3,16 +3,15 @@ module LikesHelper
     likes_profile_ids = newsfeed_item.likes.pluck(:profile_id)
     if likes_profile_ids.include?(profile.id)
       profile_like = newsfeed_item.likes.where(profile_id: profile.id)[0]
-      return link_to("<i class='fa fa-thumbs-up liked'></i>".html_safe, newsfeed_item_like_path(newsfeed_item_id: newsfeed_item.id, id: profile_like.id), method: :delete)
+      return link_to("<i class='fa fa-heart u-text--orange'></i>".html_safe, newsfeed_item_like_path(newsfeed_item_id: newsfeed_item.id, id: profile_like.id), method: :delete)
     else
-      return link_to("<i class='fa fa-thumbs-o-up'></i>".html_safe, newsfeed_item_likes_path(newsfeed_item_id: newsfeed_item.id), method: :post)
+      return link_to("<i class='fa fa-heart u-text--gray'></i>".html_safe, newsfeed_item_likes_path(newsfeed_item_id: newsfeed_item.id), method: :post)
     end
   end
 
   def like_number(newsfeed_item)
     unless newsfeed_item.likes.empty?
-      response = "#{newsfeed_item.likes.length} like"
-      response << "s" if newsfeed_item.likes.length > 1
+      response = "#{newsfeed_item.likes.length}"
       return response
     else
       return ""
