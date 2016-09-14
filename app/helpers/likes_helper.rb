4c859252fbb1e7346c8ffb0600df954986b21af4
ReidasaurusRex <reidhlewis91@gmail.com>
2016-09-14 15:06:01 -0700

    Implemented color utility name change

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -3,9 +3,9 @@ module LikesHelper
     likes_profile_ids = newsfeed_item.likes.pluck(:profile_id)
     if likes_profile_ids.include?(profile.id)
       profile_like = newsfeed_item.likes.where(profile_id: profile.id)[0]
-      return link_to("<i class='fa fa-heart u-text--orange'></i>".html_safe, newsfeed_item_like_path(newsfeed_item_id: newsfeed_item.id, id: profile_like.id), method: :delete)
+      return link_to("<i class='fa fa-heart u-text--apricot'></i>".html_safe, newsfeed_item_like_path(newsfeed_item_id: newsfeed_item.id, id: profile_like.id), method: :delete)
     else
-      return link_to("<i class='fa fa-heart u-text--gray'></i>".html_safe, newsfeed_item_likes_path(newsfeed_item_id: newsfeed_item.id), method: :post)
+      return link_to("<i class='fa fa-heart u-text--silver-sand'></i>".html_safe, newsfeed_item_likes_path(newsfeed_item_id: newsfeed_item.id), method: :post)
     end
   end
 
