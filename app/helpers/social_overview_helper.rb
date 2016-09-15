a9d1499dd66d70f62b24a7e340fea6f9b444754e
ReidasaurusRex <reidhlewis91@gmail.com>
2016-09-15 10:03:19 -0700

    Added unapprecite and appreciate titles to appreciate button, conditionally on profile appreciation of newsfeed item

--- app/helpers/social_overview_helper.rb
+++ app/helpers/social_overview_helper.rb
@@ -4,9 +4,9 @@ module SocialOverviewHelper
     likes_profile_ids = newsfeed_item.likes.pluck(:profile_id)
     if likes_profile_ids.include?(profile.id)
       profile_like = newsfeed_item.likes.where(profile_id: profile.id)[0]
-      return link_to("<i class='fa fa-heart u-text--apricot'></i>".html_safe, newsfeed_item_like_path(newsfeed_item_id: newsfeed_item.id, id: profile_like.id), method: :delete)
+      return link_to("<i class='fa fa-heart u-text--apricot'></i>".html_safe, newsfeed_item_like_path(newsfeed_item_id: newsfeed_item.id, id: profile_like.id), method: :delete, title: "Unappreciate")
     else
-      return link_to("<i class='fa fa-heart u-text--silver-sand'></i>".html_safe, newsfeed_item_likes_path(newsfeed_item_id: newsfeed_item.id), method: :post)
+      return link_to("<i class='fa fa-heart u-text--silver-sand'></i>".html_safe, newsfeed_item_likes_path(newsfeed_item_id: newsfeed_item.id), method: :post, title: "Appreciate")
     end
   end
 
