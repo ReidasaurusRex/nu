9708f217980dafb4b64528d468db9ee0cae0a2c8
ReidasaurusRex <reidhlewis91@gmail.com>
2016-09-15 18:15:44 -0700

    Added redirect for footprints#show depending on completion

--- app/controllers/profiles/footprints_controller.rb
+++ app/controllers/profiles/footprints_controller.rb
@@ -3,6 +3,7 @@ class Profiles::FootprintsController < Inheritance::ProfileMatchesUserController
 
   before_action :get_footprint, only: [:show, :destroy]
   before_action :ensure_footprint_belongs_to_user, except: [:index, :create]
+  before_action :redirect_if_incomplete, only: :show
 
   def index
     @footprints = @profile.footprints.order(created_at: :desc)
@@ -33,5 +34,18 @@ class Profiles::FootprintsController < Inheritance::ProfileMatchesUserController
       flash[:error] = "Unauthorized"
       redirect_to profile_stats_path(@profile)
     end 
+  end
+
+  def redirect_if_incomplete
+    if !@footprint.complete?
+      redirect_to footprint_create_transportation_path(footprint_id: @footprint.id) and return unless @footprint.transportation
+      redirect_to new_transportation_vehicle_path(transportation_id: @footprint.transportation.id) and return unless @footprint.transportation.vehicle
+      redirect_to new_transportation_public_transportation_path(transportation_id: @footprint.transportation.id) and return unless @footprint.transportation.public_transportation
+      redirect_to new_transportation_air_travel_path(transportation_id: @footprint.transportation.id) and return unless @footprint.transportation.air_travel
+      redirect_to new_footprint_home_energy_path(footprint_id: @footprint.id) and return unless @footprint.home_energy
+      redirect_to new_footprint_water_path(footprint_id: @footprint.id) and return unless @footprint.water
+      redirect_to new_footprint_waste_path(footprint_id: @footprint.id) and return unless @footprint.waste
+      redirect_to new_footprint_diet_path(footprint_id: @footprint.id) and return unless @footprint.diet
+    end
   end
 end
