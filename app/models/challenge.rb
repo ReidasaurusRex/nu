6d7188d02399d71240eaf7edc4bab7c0c19d5c12
ReidasaurusRex <reidhlewis91@gmail.com>
2016-09-19 11:33:14 -0700

    Added fuzzy search for progress_category and title to challenge

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -2,6 +2,8 @@ class Challenge < ActiveRecord::Base
   has_many :started_challenges
   has_many :completed_challenges
 
+  fuzzily_searchable :progress_category, :title
+
   def image_path
     "icon_color_#{self.progress_category}.png"
   end
