b7d139e44aaa94e1c8896c81384dd19f2fca472c
ReidasaurusRex <reidhlewis91@gmail.com>
2016-09-21 15:07:23 -0700

    Added conditional get profile to feedback controller

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,4 +1,6 @@
 class FeedbackController < ApplicationController
+  before_action :get_profile_if_exists
+  
   def new
   end
 
@@ -9,5 +11,12 @@ class FeedbackController < ApplicationController
 
   def thanks
     @subject = params[:subject]
+  end
+
+  private
+  def get_profile_if_exists
+    if @user.profile
+      @profile == @user.profile
+    end
   end
 end
