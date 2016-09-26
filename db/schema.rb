4f6f0da0cd8f338cf02c7c8b588a4d059b65e16c
ReidasaurusRex <reidhlewis91@gmail.com>
2016-09-26 16:09:46 -0700

    Added update number to challenges

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926230712) do
+ActiveRecord::Schema.define(version: 20160926230901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20160926230712) do
     t.integer  "difficulty"
     t.integer  "points"
     t.integer  "length"
+    t.integer  "update_num"
   end
 
   create_table "comments", force: :cascade do |t|
