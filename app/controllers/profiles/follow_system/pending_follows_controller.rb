f1079d780dd60d7287eba0d1582e7d16f9ff7ea0
ReidasaurusRex <reidhlewis91@gmail.com>
2016-09-26 11:47:04 -0700

    Revamped pending_follow#destroy so that it also destroys the pending_followers pending_subscription

--- app/controllers/profiles/follow_system/pending_follows_controller.rb
+++ app/controllers/profiles/follow_system/pending_follows_controller.rb
@@ -7,13 +7,19 @@ class Profiles::FollowSystem::PendingFollowsController < Inheritance::ProfileMat
   end
 
   def destroy
-    @pending_follow = PendingFollow.find(params[:id])
-    @pending_follow.destroy
-    redirect_to profile_path(@profile)
+    destroy_pending_follow(params[:id])
   end
 
   private
   def pending_follow_params
     params.require(:pending_follow).permit(:profile_id, :pending_follower_id)
+  end
+
+  def destroy_pending_follow(id)
+    @pending_follow = PendingFollow.find(id)
+    @pending_subscription = @pending_follow.pending_follower.pending_subscriptions.where(pending_following_id: @profile.id)[0]
+    @pending_follow.destroy
+    @pending_subscription.destroy
+    redirect_to profile_path(@profile)
   end
 end
