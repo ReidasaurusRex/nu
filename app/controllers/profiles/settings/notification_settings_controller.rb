b08a8c168714ba9e65f8966680fcc78e84bc2a2a
ReidasaurusRex <reidhlewis91@gmail.com>
2016-10-06 10:46:37 -0700

    Added form and functionality for notification_setting update

--- app/controllers/profiles/settings/notification_settings_controller.rb
+++ app/controllers/profiles/settings/notification_settings_controller.rb
@@ -5,12 +5,14 @@ class Profiles::Settings::NotificationSettingsController < Inheritance::ProfileM
 
   private
   def notification_setting_params
-    params.require(:notification_setting).permit(:location, :feed, :last_name)
+    binding.pry
+    params.fetch(:notification_setting, {}).permit(:likes, :comments, :challenge_reminders, :new_challenges)
   end
 
   def update_notification_setting(params)
     @notification_setting = @profile.notification_setting
     @notification_setting.update(params)
+    binding.pry
     redirect_to profile_general_settings_path(@profile)
   end
 end
