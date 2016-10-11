6578bfd4b04bd03565a30555471232b0e01ece8c
ReidasaurusRex <reidhlewis91@gmail.com>
2016-10-11 11:26:40 -0700

    Added conditional linear gradients on point donut graph

--- app/assets/javascripts/point_graph.js
+++ app/assets/javascripts/point_graph.js
@@ -6,7 +6,25 @@ function ready() {
 $(document).ready(ready); 
 $(document).on('page:load', ready); // turbolinks friendly
 
+
+
 function drawPointGraph(data) {
+  var chartColors = ['#B1D276', '#FFB907', '#8BD1CA', '#E64D7D', '#F1613A', '#B17FCE'];
+  // [Travel green, energy yellow, water blue, waste pink, food orange, social purple]
+  var radianVals = dataArrayFromObj(data).map(function(el, ind, ary) {
+    return  radiansFromEl(ary, ind);
+  });
+  window.radianVals = radianVals;
+  var linearGradientColors = radianVals.map(function(el, index) {
+    var color = chartColors[index];
+    return {
+      linearGradient: linearGradientsFromRadian(el),
+      stops: [
+        [0, color],
+        [1, Highcharts.Color(color).brighten(0.25).get('rgb')]
+      ]
+    };
+  }); 
   if ($('#jsPointOverviewGraph').length > 0) {
     new Highcharts.Chart({
       chart: {
@@ -15,8 +33,7 @@ function drawPointGraph(data) {
         renderTo: 'jsPointOverviewGraph',
         type: 'pie'
       }, 
-      colors: ['#B1D276', '#FFB907', '#8BD1CA', '#E64D7D', '#F1613A', '#B17FCE'], 
-      //      [Travel green, energy yellow, water blue, waste pink, food orange, social purple]
+      colors: linearGradientColors,
       credits: {
         enabled: false
       },
@@ -63,4 +80,41 @@ function extractDataPoints(data) {
 
 function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function dataArrayFromObj(dataObj) {
+  result = [];
+  for (var key in dataObj) {
+    result.push(dataObj[key]);
+  }
+  return result;
+}
+
+function arraySum(array) {
+  return array.reduce(function(prev, cur) {return prev + cur;});
+}
+
+function partialArrayTotal(array, index) {
+  if (index !== 0) {
+    return arraySum(array.slice(0, index));
+  } else {
+    return 0;
+  }
+
+}
+
+function radiansFromEl(array, index) {
+  return Math.round((partialArrayTotal(array, index) / arraySum(array)) * (2 * Math.PI) * 100) / 100;
+}
+
+function linearGradientsFromRadian(radian) {
+  if (radian < (Math.PI / 2)) {
+    return {x1: 1, y1: 0, x2: 0, y2: 1};
+  } else if (radian >= Math.PI / 2 && radian < Math.PI) {
+    return {x1: 1, y1: 1, x2: 0, y2: 0};
+  } else if (radian >= Math.PI && (3 * radian) < Math.PI / 2) {
+    return {x1: 0, y1: 1, x2: 1, y2: 0};
+  } else {
+    return {x1: 0, y1: 0, x2: 1, y2: 1};
+  }
 }
