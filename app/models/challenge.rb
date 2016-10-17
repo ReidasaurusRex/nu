5257f660381438c0452689fb9a02467eca3f910c
ReidasaurusRex <reidhlewis91@gmail.com>
2016-10-16 17:13:19 -0700

    Added a recommended_time_message to challenge.rb

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -11,4 +11,15 @@ class Challenge < ActiveRecord::Base
   def time_between_updates
     return self.length / (self.update_num - 1)
   end
+
+  def reccommended_time_message
+  time_array = [[3153600 ,"yr"], [2592000, "mth"], [86400, "d"], [3600, "hr"], [60, "m"], [1, "s"]]
+    time_array.each do |time| 
+      if (self.time_between_updates / time[0]) >= 1
+        x_since = (self.time_between_updates / time[0]).to_i
+        response = "#{x_since}#{time[1]}"
+        return response
+      end
+    end
+  end
 end
