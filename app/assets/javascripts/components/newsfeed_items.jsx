4892c098e18e6719d854876e61939292f63c3f65
ReidasaurusRex <reidhlewis91@gmail.com>
2016-10-24 15:50:12 -0700

    Added a loadTenItems function to newsfeed_items component

--- app/assets/javascripts/components/newsfeed_items.jsx
+++ app/assets/javascripts/components/newsfeed_items.jsx
@@ -1,7 +1,23 @@
 var NewsfeedItems = React.createClass({
+  getInitialState: function() {
+    return  {data: []};
+  },
+  loadTenItems: function() {
+    var currentData = this.state.data;
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: currentData.concat(data)});
+      }
+    });
+  },
   render: function() {
     return (
-      <h1>Hullo!</h1>  
+      <div className="c-newsfeed-list">
+        <h1>Hullo!</h1>
+      </div>
     );
   }
 });
