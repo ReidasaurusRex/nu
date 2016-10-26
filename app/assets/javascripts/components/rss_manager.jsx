a7829c45b6b7fd19ee732c92da98be20445c91a8
ReidasaurusRex <reidhlewis91@gmail.com>
2016-10-26 11:19:27 -0700

    Added loadFeeds and componentDidMount functions to rss_manager.jsx

--- app/assets/javascripts/components/rss_manager.jsx
+++ app/assets/javascripts/components/rss_manager.jsx
@@ -1,3 +1,19 @@
 var RSSManager = React.createClass({
-  
+  getInitialState: function() {
+    return {feeds: []};
+  },
+  loadFeeds: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'GET',
+      cache: false,
+      success: function(data) {
+        this.setState({feeds: {current: data.current, suggested: data.suggested}});
+      }.bind(this)
+    });
+  },
+  componentDidMount: function() {
+    this.loadFeeds();
+  }
 });
