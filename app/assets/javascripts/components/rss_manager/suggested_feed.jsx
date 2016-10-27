1202220be0d7b4950ef8ae43dbae466b88082ce8
ReidasaurusRex <reidhlewis91@gmail.com>
2016-10-26 19:58:24 -0700

    Added link to suggested_feed.jsx. Need to add handleSubmit functionality

--- app/assets/javascripts/components/rss_manager/suggested_feed.jsx
+++ app/assets/javascripts/components/rss_manager/suggested_feed.jsx
@@ -2,8 +2,8 @@ var SuggestedFeed = React.createClass({
   render: function() {
     return (
       <li className="c-rss-manage__section__li">
-        <h3 className="c-rss-manage__section__li__left">{this.props.title}</h3>
-        
+        <h3 className="c-rss-manage__section__li__left">{this.props.feedTitle}</h3>
+        <a href="#" onClick={this.handleSubmit} title="Add " + {this.feedTitle} className="c-rss-manage__section__li__button c-rss-manage__section__li__button--add">
       </li>      
     );
   }
