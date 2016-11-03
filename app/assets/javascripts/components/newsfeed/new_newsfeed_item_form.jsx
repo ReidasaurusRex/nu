a045878d02adb4497acd45cd715c870df682ec1c
ReidasaurusRex <reidhlewis91@gmail.com>
2016-11-03 13:22:15 -0700

    Touched empty NewNewsfeedItemForm component. Unnested comments components directory from newsfeed

---
+++ app/assets/javascripts/components/newsfeed/new_newsfeed_item_form.jsx
@@ -0,0 +1,10 @@
+var NewNewsfeedItemForm = React.createClass({
+  render: function() {
+    return (
+      <div className="o-bubble o-bubble--full o-bubble--no-pad o-bubble--with-heading c-newsfeed-composer">
+        Hello!
+      </div>
+    );
+    
+  }
+});
