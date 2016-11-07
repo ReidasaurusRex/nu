15cd823974730431f20a9572282756899b3c489d
ReidasaurusRex <reidhlewis91@gmail.com>
2016-11-07 15:06:16 -0700

    Added a conditional edit or unfollow link to native_newsfeed_menu

--- app/assets/javascripts/components/newsfeed/native_newsfeed_item/native_newsfeed_menu.jsx
+++ app/assets/javascripts/components/newsfeed/native_newsfeed_item/native_newsfeed_menu.jsx
@@ -1,4 +1,11 @@
 var NativeNewsfeedMenu = React.createClass({
+  editOrUnfollow: function() {
+    if (this.props.currentProfileID === this.props.sourceID) {
+      return (<a href="#" onClick={this.props.handleEditClick}>Edit</a>)
+    } else {
+      return (<a href="#" onClick={this.props.handleUnfollow}>Unfollow</a>)
+    }
+  },
   render: function() {
     var profileUrl = "/profiles/" + this.props.sourceID;
     return (
@@ -7,7 +14,7 @@ var NativeNewsfeedMenu = React.createClass({
           <a href={profileUrl} title="View Profile">View Profile</a>
         </li>
         <li className="c-newsfeed-menu__li">
-          Unfollow
+          {this.editOrUnfollow()}
         </li>
       </ul>
     );
