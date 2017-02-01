493cd9040c7a59c540e1c8c126a884a507b308e7
ReidasaurusRex <reidhlewis91@gmail.com>
2017-02-01 11:01:31 -0800

    Refactored EditNewsfeedForm into ES2015

--- app/assets/javascripts/Components/Newsfeed/NativeNewsfeedItems/EditNewsfeedForm.jsx
+++ app/assets/javascripts/Components/Newsfeed/NativeNewsfeedItems/EditNewsfeedForm.jsx
@@ -1,19 +1,24 @@
-var EditNewsfeedForm = React.createClass({
-  getInitialState: function() {
-    return {content: this.props.content};
-  },
-  handleContentChange: function(e) {
+class EditNewsfeedForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleContentChange = this.handleContentChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      content: this.props.content
+    }
+  }
+  handleContentChange(e) {
     this.setState({content: e.target.value});
-  },
-  handleSubmit: function(e) {
+  }
+  handleSubmit(e) {
     e.preventDefault();
     var content = this.state.content.trim();
     if (!content) {
       return;
     }
     this.props.onNewsfeedItemUpdate({newsfeed_item: {content: content}});
-  },
-  render: function() {
+  }
+  render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <textarea 
@@ -25,4 +30,4 @@ var EditNewsfeedForm = React.createClass({
       </form>
     );
   }
-});
+}
